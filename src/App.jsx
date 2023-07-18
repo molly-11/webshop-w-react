14e7a9510e7532365c55cf5c7bd7d8192def319f
Edit Selmeczi <selmeczi.e@gmail.com>
2023-07-18 15:22:25 +0200

    create landing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import './styles/App.css'
 import CreateNavbar from './components/Navbar'
+import CreateLandingPage from './components/LandinPage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
     <>
       
        <CreateNavbar/>
+       <CreateLandingPage/>
     </>
   )
 }
---
+++ src/components/LandinPage.jsx
@@ -0,0 +1,19 @@
+import "../styles/LandingPage.css"
+
+function CreateLandingPage() {
+  return (
+    <div id="landingDiv">
+      
+      <div id="landingLeft">
+        <img src="src\assets\images\bedroom.png" alt="Plants in the livingroom" />
+      </div>
+      <div id="landingRight">
+        <img src="src\assets\images\chair.jpg" alt="Indoor palmtree" />
+        <img src="src\assets\logo\logo3.svg" alt="logo" />
+        
+      </div>
+    </div>
+  );
+}
+
+export default CreateLandingPage;
