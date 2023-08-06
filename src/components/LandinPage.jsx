1d500b2c01ecec9ae70bbdc4041998660c8227e2
Edit Selmeczi <selmeczi.e@gmail.com>
2023-08-06 12:25:52 +0200

    change img path on landing page

--- src/components/LandinPage.jsx
+++ src/components/LandinPage.jsx
@@ -5,11 +5,11 @@ function CreateLandingPage() {
     <div id="landingDiv">
       
       <div id="landingLeft">
-        <img src="public\assets\images\bedroom.png" alt="Plants in the livingroom" />
+        <img src="./assets/images/bedroom.png" alt="Plants in the livingroom" />
       </div>
       <div id="landingRight">
-        <img src="public\assets\images\chair.jpg" alt="Indoor palmtree" />
-        <img src="public\assets\logo\logo.svg" alt="logo" id="mainLogo"/>
+        <img src="/assets/images/chair.jpg" alt="Indoor palmtree" />
+        <img src="public/assets/logo/logo.svg" alt="logo" id="mainLogo"/>
         
       </div>
     </div>
